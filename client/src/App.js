d8c3c2ef89d75986523b1784ad8f0e91f2aa70dc
Harshi <lharshitharebala@gmail.com>
2023-02-12 10:58:16 -0800

    app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -30,3 +30,43 @@ const App = () => {
 }
 
 export default App;
+
+/*import React, { useState } from 'react';
+import axios from 'axios';
+
+function Form() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = event => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/jwt', formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="sub" placeholder="Name" onChange={handleChange} />
+      <input type="text" name="name" placeholder="Age" onChange={handleChange} />
+      <button type="submit">Create JWT</button>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Learning JWT!</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App; */
