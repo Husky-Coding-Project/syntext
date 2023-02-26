87c71873186efbe90818033bbb2bd5d9520d34b2
elimelt <elimelt@uw.edu>
2023-02-26 11:54:23 -0800

    added cors support

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const readSnippetRouter = require('./controllers/readSnippets')
 const editSnippetRouter = require('./controllers/editSnippets')
+const cors = require('cors');
 
 // const serverURL = 'http://localhost:3001'
 // const routerURL = '/api/read'
@@ -8,6 +9,7 @@ const editSnippetRouter = require('./controllers/editSnippets')
 // const parameterURL = '?length=LONG'
 
 const app = express()
+app.use(cors());
 app.use('/api/read', readSnippetRouter)
 app.use('/api/edit', editSnippetRouter)
 
